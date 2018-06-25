8e18b18034139ce2538fe793f511c992ecacbb17
Zach Swanson <zach.j.swanson@gmail.com>
2018-06-25 09:10:37 -0700

    prep for netlify deploment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, Navbar, Nav, NavDropdown, NavItem, MenuItem } from 'react-bootstrap';
-import logo from './logo.svg';
+import { Grid, Row, Col } from 'react-bootstrap';
+import Navigation from './Navigation.js';
 import './App.css';
 
 class App extends Component {
@@ -22,39 +22,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar inverse collapseOnSelect onSelect={this.handleSelect}>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#brand">React-Bootstrap</a>
-            </Navbar.Brand>
-            <Navbar.Toggle />
-          </Navbar.Header>
-          <Navbar.Collapse>
-            <Nav>
-              <NavItem eventKey={1} href="#">
-                Link
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                Link
-              </NavItem>
-              <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                <MenuItem eventKey={3.1}>Action</MenuItem>
-                <MenuItem eventKey={3.2}>Another action</MenuItem>
-                <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                <MenuItem divider />
-                <MenuItem eventKey={3.3}>Separated link</MenuItem>
-              </NavDropdown>
-            </Nav>
-            <Nav pullRight>
-              <NavItem eventKey={1} href="#">
-                Link Right
-              </NavItem>
-              <NavItem eventKey={2} href="#">
-                Link Right
-              </NavItem>
-            </Nav>
-          </Navbar.Collapse>
-        </Navbar>
+        <Navigation handleSelect={this.handleSelect}></Navigation>
         <Grid>
           <Row>
             <Col xs={12} md={6}>
